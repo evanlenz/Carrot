af4be2bbae1e39ff83e45d38ecb54d4042df2a9d
Evan Lenz <evan@evanlenz.net>
2012-01-09 11:54:25 -0800

    Pretty-ify the results to ease debugging.

--- test/saxon/test.sh
+++ test/saxon/test.sh
@@ -1,1 +1,3 @@
 ./run-carrot.sh ../01/test.crt ../01/test.xml
+xmllint --format temp/compiled-carrot.xsl >temp/compiled-carrotPRETTY.xsl
+xmllint --format temp/parsed-carrot.xml   >temp/parsed-carrotPRETTY.xml
