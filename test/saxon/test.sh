27d881f56277ec815158a4144d4292f58d6fd59a
Evan Lenz <evan@evanlenz.net>
2013-04-15 12:37:53 -0700

    Added usage message to test script for Saxon.

--- test/saxon/test.sh
+++ test/saxon/test.sh
@@ -1,3 +1,8 @@
-./run-carrot.sh ../$1/test.crt ../$1/test.xml
-xmllint --format temp/compiled-carrot.xsl >temp/compiled-carrotPRETTY.xsl
-xmllint --format temp/parsed-carrot.xml   >temp/parsed-carrotPRETTY.xml
+#!/bin/bash
+if [ -n "$1" ]; then 
+  ./run-carrot.sh ../$1/test.crt ../$1/test.xml
+  xmllint --format temp/compiled-carrot.xsl >temp/compiled-carrotPRETTY.xsl
+  xmllint --format temp/parsed-carrot.xml   >temp/parsed-carrotPRETTY.xml
+else
+  echo "USAGE: You must specify the test directory, e.g.: ./test.sh 01"
+fi
